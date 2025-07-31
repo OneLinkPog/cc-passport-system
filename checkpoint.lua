b9f4c237c22436e78a51f74d751ca77ef2d9027d
OneLink <134339728+OneLinkPog@users.noreply.github.com>
2025-07-31 10:34:14 -0400

    Create checkpoint.lua

---
+++ checkpoint.lua
@@ -0,0 +1,33 @@
+-- checkpoint.lua
+local modem = peripheral.find("modem") or error("No modem attached")
+modem.open(300)
+
+local countryCode = "BLIS" -- Country this checkpoint is located in
+
+while true do
+  print("Enter player name:")
+  local name = read()
+
+  print("Enter passport code:")
+  local code = read()
+
+  -- First, validate that the code matches the name
+  modem.transmit(100, 300, {action = "get_passport_info", code = code})
+  local _, _, _, _, response = os.pullEvent("modem_message")
+
+  if response.status ~= "success" then
+    print("Passport not found.")
+  elseif response.info.name ~= name then
+    print("Name does not match passport!")
+  else
+    -- Now check visa
+    modem.transmit(100, 300, {action = "check_visa", code = code, target = countryCode})
+    _, _, _, _, response = os.pullEvent("modem_message")
+
+    if response.allowed then
+      print("Access Granted to", name)
+    else
+      print("Access Denied. No visa for this country.")
+    end
+  end
+end
