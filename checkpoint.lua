3062cd37af1e211b9da705b9c2bb3d07f4651571
OneLink <134339728+OneLinkPog@users.noreply.github.com>
2025-07-31 11:51:25 -0400

    Update checkpoint.lua

--- checkpoint.lua
+++ checkpoint.lua
@@ -1,101 +1,40 @@
--- checkpoint.lua
--- Run this on a checkpoint computer with player detector and modem
-local COUNTRY_CODE = "BLIS"  -- Replace with the code for this country
+local modem = peripheral.find("modem") or error("No modem attached")
+modem.open(300)
 
--- Get peripherals
-local modem = peripheral.find("modem") or error("No modem found")
-local detector = peripheral.find("playerDetector") or error("No player detector found")
+local countryCode = "BLIS" -- Country this checkpoint is located in
 
--- Utility functions
-local function getUsername()
-    local players = detector.getOnlinePlayers()
-    if #players == 0 then
-        print("No players nearby.")
-        return nil
-    elseif #players > 1 then
-        print("Multiple players detected. Closest one will be used.")
-    end
-
-    -- Sort players by distance
-    table.sort(players, function(a, b)
-        return a.distance < b.distance
-    end)
-
-    return players[1].name
-end
-
-local function requestPassportCode(username)
-    modem.transmit(1001, 1001, {
-        type = "get_passport",
-        username = username
-    })
-
-    local timer = os.startTimer(3)
-    while true do
-        local event, side, channel, replyChannel, message = os.pullEvent()
-        if event == "modem_message" and channel == 1001 and type(message) == "table" then
-            if message.type == "passport_data" and message.username == username then
-                return message.passportCode, message.data
-            end
-        elseif event == "timer" and side == timer then
-            print("Passport server did not respond.")
-            return nil, nil
-        end
-    end
-end
-
-local function logAccess(passportCode, username, allowed)
-    modem.transmit(1001, 1001, {
-        type = "log_access",
-        passportCode = passportCode,
-        username = username,
-        allowed = allowed,
-        location = COUNTRY_CODE,
-        timestamp = os.epoch("utc")
-    })
-end
-
--- Main Loop
 while true do
-    print("\n--- Passport Checkpoint ---")
-    print("Waiting for player...")
+  print("Enter player name:")
+  local name = read()
 
-    local username = getUsername()
-    if not username then
-        sleep(2)
-        goto continue
-    end
+  print("Enter passport code:")
+  local code = read()
 
-    print("Detected player: " .. username)
+  -- First, get passport info and verify name
+  modem.transmit(100, 300, {action = "get_passport_info", code = code})
+  local _, _, _, _, response = os.pullEvent("modem_message")
 
-    local passportCode, passportData = requestPassportCode(username)
+  if response.status ~= "success" then
+    print("Passport not found.")
+  elseif response.info.name ~= name then
+    print("Name does not match passport!")
+  else
+    local nationality = response.info.nationality
 
-    if passportData then
-        if passportData.nationality == COUNTRY_CODE then
-            print("Welcome home, " .. username .. "!")
-            logAccess(passportCode, username, true)
-        else
-            local hasVisa = false
-            for _, visa in ipairs(passportData.visas) do
-                if visa == COUNTRY_CODE then
-                    hasVisa = true
-                    break
-                end
-            end
-
-            if hasVisa then
-                print("Access granted (visa found).")
-                logAccess(passportCode, username, true)
-            else
-                print("Access denied (no visa for " .. COUNTRY_CODE .. ").")
-                logAccess(passportCode, username, false)
-            end
-        end
+    if nationality == countryCode then
+      -- Citizen: welcome home, access granted without visa
+      print("Welcome home, " .. name .. "!")
+      print("Access Granted to " .. name)
     else
-        print("Invalid passport.")
-        logAccess("unknown", username, false)
-    end
+      -- Non-citizen: check visa
+      modem.transmit(100, 300, {action = "check_visa", code = code, target = countryCode})
+      _, _, _, _, response = os.pullEvent("modem_message")
 
-    sleep(2)
-    ::continue::
+      if response.allowed then
+        print("Access Granted to " .. name)
+      else
+        print("Access Denied. No visa for this country.")
+      end
+    end
+  end
 end
