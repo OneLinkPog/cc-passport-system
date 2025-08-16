13447a39dccd2816feb927e6ccd05b119c052f20
OneLink <134339728+OneLinkPog@users.noreply.github.com>
2025-08-16 13:59:32 -0400

    Create visa_issuer.lua

---
+++ visa_issuer.lua
@@ -0,0 +1,20 @@
+-- visa_issuer.lua
+local modem = peripheral.find("modem") or error("No modem attached")
+modem.open(400)
+local country = "BLIS"
+
+print("Visa Issuing Console")
+while true do
+  print("Issuing visas for country:", country)
+  print("Enter passport code:")
+  local code = read()
+
+  modem.transmit(100, 400, {action = "add_visa", code = code, target = country})
+  local _, _, _, _, response = os.pullEvent("modem_message")
+
+  if response.status == "visa_added" then
+    print("Visa granted for", code, "in country", target)
+  else
+    print("Error")
+  end
+end
